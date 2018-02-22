e14b5cefc0ba5f568e61173f0145d0fcac142ea3
Tyler White <tylervw51@gmail.com>
2018-02-22 00:56:32 +0000

    Fixed a few problems with the addon analysis

--- usage_report/utils/top10addons.py
+++ usage_report/utils/top10addons.py
@@ -30,7 +30,7 @@ addon_filter = (~col('addon.is_system')) & (~col('addon.foreign_install')) & \
     (~col('addon.addon_id').like('%@shield.mozilla%')) & (~col('addon.addon_id').like('%' + UNIFIED_SEARCH_STR + '%'))
 
     
-def top10AddonsOnDate(data, date, topN):
+def top10AddonsOnDate(data, date, topN, country_list):
     """ Gets the number of users in the past week who have used the top N addons,
         broken down by country.
         
@@ -43,6 +43,17 @@ def top10AddonsOnDate(data, date, topN):
         Returns:
         Dataframe containing the number of users using each of the addons.
     """
+    if country_list is not None:
+        data2 = data.drop('country').select('*', lit('All').alias('country'))
+        data3 = data.filter(col('country').isin(country_list))\
+                    .select('*', col('country').alias('country2'))\
+                    .drop('country')\
+                    .select('*', col('country2').alias('country'))\
+                    .drop('country2')
+        data = data2.union(data3)
+    else:
+        data = data.drop('country').select('*', lit('All').alias('country'))
+        
     start_date = (date - pd.Timedelta(days=7)).strftime('%Y%m%d')
 
     wau = data.filter((col('submission_date_s3') > start_date) & 
@@ -56,22 +67,29 @@ def top10AddonsOnDate(data, date, topN):
         .filter((col('submission_date_s3') > start_date) & 
                 (col('submission_date_s3') <= date.strftime('%Y%m%d')))\
         .filter(addon_filter)\
-        .select('country', 'client_id', 'addon.name')\
+        .select('country', 'client_id', 'addon.addon_id', 'addon.name')\
         .distinct()\
-        .groupBy('country', 'name')\
-        .agg(F.count('*').alias('number_of_users'))\
+        .groupBy('country', 'addon_id')\
+        .agg(F.count('*').alias('number_of_users'), F.last('name').alias('name'))\
         .select('*', lit(date.strftime('%Y%m%d')).alias('submission_date_s3'),
                 lit(start_date).alias('start_date'),
-                F.rank().over(Window.partitionBy('country')\
-                              .orderBy(desc('number_of_users')))\
+                F.row_number().over(Window.partitionBy('country')\
+                                      .orderBy(desc('number_of_users'))\
+                                      .rowsBetween(Window.unboundedPreceding, Window.currentRow))
                               .alias('rank'))\
         .filter(col('rank') <= topN)\
         
-    return counts.join(F.broadcast(wau), on = ['submission_date_s3', 'country'])\
-        .select('country', 'submission_date_s3', 'start_date', 'name', 
-                (col('number_of_users') / col('wau')).alias('percent_of_active_users'), 
-                'rank', 'number_of_users', 'wau')
-    
+    if country_list is not None:
+        return counts.join(F.broadcast(wau), on = ['country'])\
+            .select('country', 'submission_date_s3', 'start_date', 'addon_id', 'name', 
+                    (col('number_of_users') / col('wau')).alias('percent_of_active_users'), 
+                    'rank', 'number_of_users', 'wau')
+    else:
+        wau_num = wau.toPandas()['wau'][0]
+        return counts.select('country', 'submission_date_s3', 'start_date', 'addon_id', 'name', 
+                    (col('number_of_users') / wau_num).alias('percent_of_active_users'), 
+                    'rank', 'number_of_users', lit(wau_num).alias('wau'))
+
 
 def top10Addons(data, start_date, end_date, country_list, sc, topN = 10):
     """ Gets the number of users in the past week who have used the top N addons,
@@ -90,13 +108,8 @@ def top10Addons(data, start_date, end_date, country_list, sc, topN = 10):
     start_date = pd.to_datetime(start_date, format = '%Y%m%d')
     end_date = pd.to_datetime(end_date, format = '%Y%m%d')
     dates = pd.date_range(start_date, end_date, freq = '7D')
-    
-    if country_list is None:
-        data = data.drop('country').select('*', lit('All').alias('country'))
-    else:
-        data = data.filter(col('country').isin(country_list))
-    
-    outputs = [top10AddonsOnDate(data, date, topN) 
+
+    outputs = [top10AddonsOnDate(data, date, topN, country_list) 
                    for date in dates]
     
     return sc.union([output.rdd for output in outputs]).toDF()
