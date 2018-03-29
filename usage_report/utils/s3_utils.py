7542da242e4506a29ae05f464b1be8c1b8adde46
SuYoungHong <shong@mozilla.com>
2018-03-28 19:59:41 -0700

    minor CR fixes

--- usage_report/utils/s3_utils.py
+++ usage_report/utils/s3_utils.py
@@ -2,17 +2,6 @@ import boto3
 import json
 
 
-def create_session(aws_access_key_id=None, aws_secret_access_key=None):
-    """ create boto3 session with access id and secret key
-    params: aws_access_key_id, aws_secret_access_key, if None it should check env
-    """
-    session = boto3.Session(
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
-    )
-    return session
-
-
 def file_exists(bucket_name, filename, aws_access_key_id=None, aws_secret_access_key=None):
     """ check if a file exists in S3
     params: bucket_name, str, name of bucket
@@ -20,11 +9,14 @@ def file_exists(bucket_name, filename, aws_access_key_id=None, aws_secret_access
     aws_access_key_id, aws_secret_access_key, if None it should check env
     return: True if file exists
     """
-    s3 = create_session(aws_access_key_id, aws_secret_access_key).resource('s3')
+    s3 = boto3.Session(aws_access_key_id=aws_access_key_id,
+                       aws_secret_access_key=aws_secret_access_key).resource('s3')
     bucket = s3.Bucket(bucket_name)
     objs = list(bucket.objects.filter(Prefix=filename))
     if len(objs) > 0 and objs[0].key == filename:
         return True
+    else:
+        return False
 
 
 def read_from_s3(bucket_name, filename, aws_access_key_id=None, aws_secret_access_key=None):
@@ -34,11 +26,11 @@ def read_from_s3(bucket_name, filename, aws_access_key_id=None, aws_secret_acces
     return: JSON as dict, None if file doesn't exist in S3
     """
     if file_exists(bucket_name, filename, aws_access_key_id, aws_secret_access_key):
-        s3 = create_session(aws_access_key_id, aws_secret_access_key).resource('s3')
+        s3 = boto3.Session(aws_access_key_id=aws_access_key_id,
+                           aws_secret_access_key=aws_secret_access_key).resource('s3')
         content_object = s3.Object(bucket_name, filename)
         file_content = content_object.get()['Body'].read().decode('utf-8')
-        json_content = json.loads(file_content)
-        return json_content
+        return json.loads(file_content)
 
 
 def write_to_s3(bucket_name, filename, data, aws_access_key_id=None, aws_secret_access_key=None):
@@ -47,6 +39,7 @@ def write_to_s3(bucket_name, filename, data, aws_access_key_id=None, aws_secret_
     filename, str, name of file (prefix + file name)
     return: nothing
     """
-    s3 = create_session(aws_access_key_id, aws_secret_access_key).resource('s3')
+    s3 = boto3.Session(aws_access_key_id=aws_access_key_id,
+                       aws_secret_access_key=aws_secret_access_key).resource('s3')
     obj = s3.Object(bucket_name, filename)
     obj.put(Body=json.dumps(data, ensure_ascii=False).encode('utf8'))
