de09a34a575fae2a8992c19f5ccf6ebd541d9979
Harold Woo <hwoo@mozilla.com>
2019-09-24 17:39:42 -0700

    Modifying helper script to read from gcs bucket rather than s3 when using dataproc

--- usage_report/utils/helpers.py
+++ usage_report/utils/helpers.py
@@ -27,19 +27,19 @@ def keep_countries_and_all(data, country_list):
     return data_all
 
 
-def get_dest(output_bucket, output_prefix, output_version, spark_provider='emr',
-             date=None, sample_id=None):
+def get_dest(bucket, prefix, version, spark_provider='emr', date=None, sample_id=None):
     '''
-    Stiches together an s3 destination.
-    :param output_bucket: s3 output_bucket
-    :param output_prefix: s3 output_prefix (within output_bucket)
-    :param output_version: dataset output_version
-    :retrn str ->
-    s3(a)://output_bucket/output_prefix/output_version/submissin_date_s3=[date]/sample_id=[sid]
+    Stiches together an s3 or gcs destination.
+    :param bucket: s3 or gcs bucket
+    :param prefix: s3 or gcs prefix (within bucket)
+    :param version: dataset version
+    :param spark_provider: either 'emr' or 'dataproc'
+    :return str ->
+    s3|gs://bucket/prefix/version/submission_date_s3=[date]/sample_id=[sid]
     '''
 
     if spark_provider == 'dataproc':
-        prefix = 's3a://'
+        prefix = 'gs://'
     else:
         prefix = 's3://'
 
@@ -48,7 +48,7 @@ def get_dest(output_bucket, output_prefix, output_version, spark_provider='emr',
         suffix += "/submission_date_s3={}".format(date)
     if sample_id is not None:
         suffix += "/sample_id={}".format(sample_id)
-    full_dest = prefix + '/'.join([output_bucket, output_prefix, output_version]) + suffix + '/'
+    full_dest = prefix + '/'.join([bucket, prefix, version]) + suffix + '/'
     return full_dest
 
 
@@ -60,8 +60,10 @@ def load_main_summary(spark, input_bucket, input_prefix, input_version, spark_pr
     :param input_bucket: s3 bucket (telemetry-parquet)
     :param input_prefix: s3 prefix (main_summary)
     :param input_version: dataset version (v4)
+    :param spark_provider: either 'emr' or 'dataproc'
     :return SparkDF
     '''
+
     dest = get_dest(input_bucket, input_prefix, input_version, spark_provider)
     return (spark
             .read
